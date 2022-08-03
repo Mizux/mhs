38f161759ad33010618d1807e491a6ccd0c43ee7
Mizux Seiha <mizux.dev@gmail.com>
2022-08-04 01:35:23 +0200

    Update data.js

--- scripts/data.js
+++ scripts/data.js
@@ -1,8 +1,15 @@
-// Atk among: Speed > Power > Technical > Speed
+// Atk: Speed > Power > Technical > Speed
+// Weakness: Slash | Pierce | Blunt
 let data = [
-  ['id', 'name', 'logo', 'Normal Atk', 'Special Atk', 'Notes'],
-  ['001', 'Aptonoth', 'logo', 'Power', '', ''],
-  ['002', 'Larinoth', 'logo', 'Technical', '', ''],
+  ['id', 'name', 'Normal Atk', 'Special Atk', 'Weakness', 'Notes'],
+  ['000', 'Unknow', 'Power Technical Speed', 'Power Technical Speed', 'Slash Pierce Blunt', ''],
+  ['001', 'Aptonoth', 'Power', '', '', ''],
+  ['002', 'Larinoth', 'Technical', '', '', ''],
+  ['004', 'Velociprey', 'Speed', '', '', ''],
+  ['005', 'Velocidrone', 'Speed', 'Force', '', ''],
+  ['016', 'Fangi', 'Speed', '', '', ''],
+  ['018', 'Grand Fangia', 'Technical', 'Speed', '', ''],
+  ['022', 'Azuros', 'Power', 'Technical', '', ''],
 ];
 
 export default data;
