977b3463e662576d92b64df60e47138f2b6d67da
Mizux <mizux.dev@gmail.com>
2022-08-04 21:48:44 +0200

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -5,8 +5,8 @@ import generateTable from "./table.js";
 function redraw() {
   document.body.appendChild(generateTable(data));
   //const node = document.getElementById("main-div")
-  //node.replaceChildren(generate_table(data));
-  //console.log(generate_table(data))
+  //node.replaceChildren(generateTable(data));
+  //console.log(generateTable(data))
 }
 
 redraw();
