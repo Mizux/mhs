ce7f88b1a7df2a29fa27f87caa1cbe9eacbbffd0
Corentin Le Molgat <corentinl@google.com>
2022-08-04 11:38:51 +0200

    rename Atk column

--- scripts/data.js
+++ scripts/data.js
@@ -1,7 +1,7 @@
 // Atk: speed > power > technical > speed
 // Weapon Weakness: slash | pierce | blunt
 let data = [
-  ['ID', 'Logo', 'Name', 'Normal Atk', 'Enraged Atk', 'Weapon Weakness', 'Notes'],
+  ['ID', 'Logo', 'Name', 'Normal', 'Enraged', 'Weapon Weakness', 'Notes'],
   ['000', 'Unknow', 'power technical speed', 'power technical speed', ['slash pierce blunt', ['Part', 'slash pierce blunt']], 'note'],
   ['001', 'Aptonoth', 'power', '', ['slash pierce'], ''],
   ['002', 'Larinoth', 'technical', '', ['slash pierce'], ''],
